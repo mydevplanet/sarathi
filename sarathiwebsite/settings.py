00978a92769a21398ef0775068f837dc23e6a427
Dev Planet <mydevplanet@gmail.com>
2023-12-30 23:18:39 +0530

    second commit

--- sarathiwebsite/settings.py
+++ sarathiwebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%hb3fatd5z@+ip#!g6%hrrjeejgzicfg%h)##s@1mu-p(b4ypk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'www.sarathijansewafoundation.com']
 
 
 # Application definition
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR/ 'static'
+
