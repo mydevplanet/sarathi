58567794467ca0f906493aa4afcb769071d135b8
Dev Planet <mydevplanet@gmail.com>
2024-01-02 06:46:27 +0530

    environ variable

--- sarathiwebsite/settings.py
+++ sarathiwebsite/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'sarathiwebsite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
