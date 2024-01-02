7f8d7b0cbefcea2491e0e998d4556f765369c91a
Dev Planet <mydevplanet@gmail.com>
2024-01-02 08:10:49 +0530

    Please enter the commit message for your changes. Lines starting extra

--- static/sarathiwebsite/settings.py
+++ static/sarathiwebsite/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'sarathiwebsite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    # "default": {
-    #     "ENGINE": "django.db.backends.mysql",
-    #     "NAME": os.environ.get('DB_NAME'),
-    #     "USER": os.environ.get('DB_USER'),
-    #     "PASSWORD": os.environ.get('DB_PASSWORD'),
-    #     "HOST": "localhost",
-    #     "PORT": "3306",
-    # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
 }
 
 
