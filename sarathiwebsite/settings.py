f5dfb17df3d429c897861594696f44875e33dc12
Dev Planet <mydevplanet@gmail.com>
2024-01-22 08:29:38 +0530

    server issue

--- sarathiwebsite/settings.py
+++ sarathiwebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%hb3fatd5z@+ip#!g6%hrrjeejgzicfg%h)##s@1mu-p(b4ypk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.sarathijansewafoundation.com', 'sarathijansewafoundation.com', 'localhost']
 
